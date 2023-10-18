52a7d93c3a1f6689d73e54dec785faf95ddd754c
muhissadik <111856721+muhissadik@users.noreply.github.com>
2023-10-18 17:25:17 +0300

    Pohja tehty

--- src/components/Kurssit.js
+++ src/components/Kurssit.js
@@ -10,9 +10,29 @@ const Kurssit = () => {
         <div className="kurssit">
         <Navbar showLinks={showLinks} setShowLinks={setShowLinks} />
         </div>
+        <div class="otsikko">
+        <h1>Kurssitarjotin</h1>
+        </div>
+      <div class="kurssitarjonta">
+        <div class="row">
+        <div class="box">
+          <p>Intensiivinen Kurssi</p>
+          <p>3,5 viikkoa</p>
+        </div>
+        <div class="box">Super Kurssi</div>
+        <div class="box">Super-takuu Kurssi</div>
+        </div>
+       <div class="kurssitarjotin">
+        <div class="row">
+        <div class="box">Hakemuspalvelu + matka</div>
+        <div class="box">Hakemuspalvelu</div>
+        <div class="box">Knots & suture</div>
+        </div>
+        </div>
     </div>
-
+    </div>
     );
 };
+
 
 export default Kurssit;
