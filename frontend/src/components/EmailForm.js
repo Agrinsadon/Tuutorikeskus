b0d46934c5da9fcd3eba170db2bb222375c68897
Agrin <agrins@metropolia.fi>
2023-10-22 21:06:16 +0300

    f

--- frontend/src/components/EmailForm.js
+++ frontend/src/components/EmailForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import '../styles/Emailform.css';
+import 'react-toastify/dist/ReactToastify.css'; // Import the CSS
+import '../styles/Emailform.css'
 
 function EmailForm() {
   const [formData, setFormData] = useState({
@@ -13,34 +13,25 @@ function EmailForm() {
   });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { email, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value,
+      [email]: value,
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const emailData = {
-      name: formData.name,
-      email: formData.email,
-      phone: formData.phone,
-      subject: formData.subject,
-      message: formData.message,
-    };
-
-    fetch('https://tuutorikeskus.onrender.com/email/send-email', {
+    fetch('https://tuutorikeskus.onrender.com/email/send-email', formData, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(emailData),
+      body: JSON.stringify(formData),
     })
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error(`Request failed with status ${response.status}`);
         }
         return response.json();
       })
@@ -63,66 +54,65 @@ function EmailForm() {
         toast.error('Viestiä ei voitu lähettää!', { position: 'top-right' });
       });
   };
+  
 
   return (
     <div>
-      <div className="ota-yhteyttä">
-        <h1>Ota Yhteyttä</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="inputs">
-            <input
-              type="text"
-              name="name"
-              placeholder="Nimi*"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="inputs">
-            <input
-              type="email"
-              name="email"
-              placeholder="Sähköposti*"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="inputs">
-            <input
-              type="text"
-              name="phone"
-              placeholder="Puhelin"
-              value={formData.phone}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="inputs">
-            <input
-              type="text"
-              name="subject"
-              placeholder="Otsikko*"
-              value={formData.subject}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="inputs">
-            <textarea
-              name="message"
-              placeholder="Viesti*"
-              value={formData.message}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <button className="button-lähetä" type="submit">
-              Lähetä
-            </button>
-          </div>
-        </form>
+      <div className='ota-yhteyttä'>
+      <h1>Ota Yhteyttä</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='inputs'>
+          <input
+            type="text"
+            name="name"
+            placeholder="Nimi*"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className='inputs'>
+          <input
+            type="email"
+            name="email"
+            placeholder="Sähköposti*"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className='inputs'>
+          <input
+            type="text"
+            name="phone"
+            placeholder="Puhelin"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+        </div>
+        <div className='inputs'>
+          <input
+            type="text"
+            name="subject"
+            placeholder="Otsikko*"
+            value={formData.subject}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className='inputs'>
+          <textarea
+            name="message"
+            placeholder="Viesti*"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <button className='button-lähetä' type="submit">Lähetä</button>
+        </div>
+      </form>
       </div>
       <ToastContainer position="top-right" autoClose={3000} />
     </div>
