2ec5235260a4faaf5914b1f553bae5adc8158ebf
Agrin <agrins@metropolia.fi>
2023-10-22 19:04:55 +0300

    ghgh

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 require('dotenv').config();
 const express = require('express');
 const emailRoutes = require('./router/EmailRoutes');
+const path = require('path'); // Import the path module.
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,14 @@ const port = process.env.PORT || 3000;
 // Use the email route module
 app.use(cors());
 app.use('/email', emailRoutes);
+
+// Serve static files from the 'build' directory.
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Define a catch-all route to serve the 'index.html' for all other routes.
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
