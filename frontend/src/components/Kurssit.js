d668c8aaab8e944b98b78796d04ad080dbaf29c9
Agrin <agrins@metropolia.fi>
2023-10-26 17:39:44 +0300

    dasdawd

--- frontend/src/components/Kurssit.js
+++ frontend/src/components/Kurssit.js
@@ -1,118 +1,106 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Navbar from './Navbar';
 import '../styles/Kurssit.css';
 
 const Kurssit = () => {
     const [showLinks, setShowLinks] = useState(false);
+    const history = useHistory();
+
+    const handleIlmottauduButtonClick = () => {
+      history.push('/Kurssit');
+
+    };
   
     return (
-      <div class="kurssitsivu">
-        <div className="kurssit">
+      <div className="kurssitsivu">
         <Navbar showLinks={showLinks} setShowLinks={setShowLinks} />
+        <div className="kurssit">
+          <h1>Kurssit</h1>
+        <div className='kurssi-card-container'>
+        <div className="kurssi-card">
+        <div className="kurssi-pic-frame">
+          <img src="Aboutus.png" alt=" " className="kurssi-card-pic" />
         </div>
-        <div className="kurssiotsikko">
-        <h1>Kurssitarjotin</h1>
+          <div className="kurssi-header">
+          <h2>Intensiivinenkurssi</h2>
+          <p>3,5 viikkoa</p>
+          </div>
+          <div className='kurssi-text'>
+          <p>
+          <span className="p-with-dots"></span>Sisältää 54 opetustuntia
+          <br />
+          <span className="p-with-dots"></span>Hakemusprosessissa avustaminen
+          <br />
+          <span className="p-with-dots"></span>Matkustamme kanssasi Romaniaan
+          <br />
+          <span className="p-with-dots"></span>Hakemusprosessissa avustaminen
+          <br />
+          <span className="p-with-dots"></span>Hakemusprosessissa avustaminen
+          </p>
+          </div>
+          <div className="ilmottaudu-button-container">
+          <span className="price-label">2000€</span>
+          <button className="ilmottaudu-button" onClick={handleIlmottauduButtonClick}>Ilmottaudu</button>
+          </div>
         </div>
 
-        <div className="row">
-        <div className="box">
-          <div className="otsikko">
-             <img src="https://media.healthecareers.com/wp-content/uploads/2022/02/11211459/medicalschool-3.jpg" alt="Example" class="kuva"></img>
-             </div>
-          <span className='teksti1'>Intensiivinen Kurssi</span>
-          <span className='teksti2'> 3,5 viikkoa </span>
-          <div className="teksti">
-          <p>1. Intensiivikurssi sisältää 54 opetustuntia.</p>
-          <p> 2. Hakemusprosessissa avustaminen.</p>
-          <p>3. Matkustamme kanssasi Romaniaan.</p>
+        <div className="kurssi-card">
+        <div className="kurssi-pic-frame">
+          <img src="Aboutus.png" alt=" " className="kurssi-card-pic" />
+        </div>
+          <div className="kurssi-header">
+          <h2>Intensiivinenkurssi</h2>
+          <p>3,5 viikkoa</p>
           </div>
-         
-          <span className="hinta">2400€</span>
-          <span><button className="info-button">Ilmoittaudu</button></span>
-     
-            </div>
-            <div className="box">
-          <div className="otsikko">
-             <img src="https://media.healthecareers.com/wp-content/uploads/2022/02/11211459/medicalschool-3.jpg" alt="Example" class="kuva"></img>
-             </div>
-          <span className='teksti1'>Intensiivinen Kurssi</span>
-          <span className='teksti2'> 3,5 viikkoa </span>
-          <div className="teksti">
-          <p>1. Intensiivikurssi sisältää 54 opetustuntia.</p>
-          <p> 2. Hakemusprosessissa avustaminen.</p>
-          <p>3. Matkustamme kanssasi Romaniaan.</p>
+          <div className='kurssi-text'>
+          <p>
+          <span className="p-with-dots"></span>Sisältää 54 opetustuntia
+          <br />
+          <span className="p-with-dots"></span>Hakemusprosessissa avustaminen
+          <br />
+          <span className="p-with-dots"></span>Matkustamme kanssasi Romaniaan
+          <br />
+          <span className="p-with-dots"></span>Hakemusprosessissa avustaminen
+          <br />
+          <span className="p-with-dots"></span>Hakemusprosessissa avustaminen
+          </p>
           </div>
-          <div className='info-container'>
-          <span className="hinta">2400€</span>
-          <span><button className="info-button">Ilmoittaudu</button></span>
-            </div>
-            </div>
-            <div className="box">
-          <div className="otsikko">
-             <img src="https://media.healthecareers.com/wp-content/uploads/2022/02/11211459/medicalschool-3.jpg" alt="Example" class="kuva"></img>
-             </div>
-          <span className='teksti1'>Intensiivinen Kurssi</span>
-          <span className='teksti2'> 3,5 viikkoa </span>
-          <div className="teksti">
-          <p>1. Intensiivikurssi sisältää 54 opetustuntia.</p>
-          <p> 2. Hakemusprosessissa avustaminen.</p>
-          <p>3. Matkustamme kanssasi Romaniaan.</p>
-          </div>
-          <div className='info-container'>
-          <span className="hinta">2400€</span>
-          <span><button className="info-button">Ilmoittaudu</button></span>
-            </div>
-            </div>
-            <div className="box">
-          <div className="otsikko">
-             <img src="https://media.healthecareers.com/wp-content/uploads/2022/02/11211459/medicalschool-3.jpg" alt="Example" class="kuva"></img>
-             </div>
-          <span className='teksti1'>Intensiivinen Kurssi</span>
-          <span className='teksti2'> 3,5 viikkoa </span>
-          <div className="teksti">
-          <p>1. Intensiivikurssi sisältää 54 opetustuntia.</p>
-          <p> 2. Hakemusprosessissa avustaminen.</p>
-          <p>3. Matkustamme kanssasi Romaniaan.</p>
-          </div>
-          <div className='info-container'>
-          <span className="hinta">2400€</span>
-          <span><button className="info-button">Ilmoittaudu</button></span>
-            </div>
-            </div>
-            <div className="box">
-          <div className="otsikko">
-             <img src="https://media.healthecareers.com/wp-content/uploads/2022/02/11211459/medicalschool-3.jpg" alt="Example" class="kuva"></img>
-             </div>
-          <span className='teksti1'>Intensiivinen Kurssi</span>
-          <span className='teksti2'> 3,5 viikkoa </span>
-          <div className="teksti">
-          <p>1. Intensiivikurssi sisältää 54 opetustuntia.</p>
-          <p> 2. Hakemusprosessissa avustaminen.</p>
-          <p>3. Matkustamme kanssasi Romaniaan.</p>
-          </div>
-          <div className='info-container'>
-          <span className="hinta">2400€</span>
-          <span><button className="info-button">Ilmoittaudu</button></span>
-            </div>
-            </div>
-            <div className="box">
-          <div className="otsikko">
-             <img src="https://media.healthecareers.com/wp-content/uploads/2022/02/11211459/medicalschool-3.jpg" alt="Example" class="kuva"></img>
-             </div>
-          <span className='teksti1'>Intensiivinen Kurssi</span>
-          <span className='teksti2'> 3,5 viikkoa </span>
-          <div className="teksti">
-          <p>1. Intensiivikurssi sisältää 54 opetustuntia.</p>
-          <p> 2. Hakemusprosessissa avustaminen.</p>
-          <p>3. Matkustamme kanssasi Romaniaan.</p>
-          </div>
-          <div className='info-container'>
-          <span className="hinta">2400€</span>
-          <span><button className="info-button">Ilmoittaudu</button></span>
-            </div>
-            </div>
+          <div className="ilmottaudu-button-container">
+          <span className="price-label">2000€</span>
+          <button className="ilmottaudu-button" onClick={handleIlmottauduButtonClick}>Ilmottaudu</button>
           </div>
         </div>
+
+        <div className="kurssi-card">
+        <div className="kurssi-pic-frame">
+          <img src="Aboutus.png" alt=" " className="kurssi-card-pic" />
+        </div>
+          <div className="kurssi-header">
+          <h2>Intensiivinenkurssi</h2>
+          <p>3,5 viikkoa</p>
+          </div>
+          <div className='kurssi-text'>
+          <p>
+          <span className="p-with-dots"></span>Sisältää 54 opetustuntia
+          <br />
+          <span className="p-with-dots"></span>Hakemusprosessissa avustaminen
+          <br />
+          <span className="p-with-dots"></span>Matkustamme kanssasi Romaniaan
+          <br />
+          <span className="p-with-dots"></span>Hakemusprosessissa avustaminen
+          <br />
+          <span className="p-with-dots"></span>Hakemusprosessissa avustaminen
+          </p>
+          </div>
+          <div className="ilmottaudu-button-container">
+          <span className="price-label">2000€</span>
+          <button className="ilmottaudu-button" onClick={handleIlmottauduButtonClick}>Ilmottaudu</button>
+          </div>
+        </div>
+        </div>
+        </div>
+      </div>
     );
 };
 
