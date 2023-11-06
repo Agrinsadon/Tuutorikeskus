52dd461470eddbd0eb4bfa1e4a1c7f07d758d326
Agrin <agrins@metropolia.fi>
2023-11-06 10:31:06 +0200

    ddd

--- frontend/src/components/Etusivu.js
+++ frontend/src/components/Etusivu.js
@@ -31,6 +31,7 @@ const Etusivu = () => {
             <br />
           </div>
           <div className="image-container">
+          <br /><br /><br /><br /><br /><br /><br />
             <img src="Video.png" alt=" " className="medical-image" loading="lazy" />
             <Video />
           </div>
