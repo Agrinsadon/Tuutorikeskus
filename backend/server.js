0739ff0c216e0f5789dada98ef88c57a4c57e47e
Agrin <agrins@metropolia.fi>
2023-11-06 10:23:53 +0200

    ssss

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const enrollRoutes = require('./router/EnrollmentRoutes')
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/Etusivu.css', function(req, res) {
+  res.setHeader('Content-Type', 'text/css');
+});
+
 app.use(cors());
 app.use('/email', emailRoutes);
 app.use('/enroll', enrollRoutes);
