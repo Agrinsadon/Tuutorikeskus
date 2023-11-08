1933c93b596048fc2086912bc6a1405a7710f78c
Agrin <agrins@metropolia.fi>
2023-11-08 15:53:09 +0200

    dsd

--- frontend/src/components/LääkeRomania.js
+++ frontend/src/components/LääkeRomania.js
@@ -17,9 +17,25 @@ const Lääketiede = () => {
           <img src="lääketiede.jpg" alt=" " className="lääketiede-card-pic" loading='lazy' />
         </div>
           <div className='lääketiede-text'>
-          <p>asfdafdasf </p>
+          <p> Moni suomalainen opiskelija pohtii mahdollisuuksiaan jatkaa opintojaan ulkomailla. Yhä useampi opiskelija suuntaa ulkomaille eri lääketieteellisiin yliopistoihin kansainvälistymismahdollisuuksia etsien. Jos olet harkinnut mahdollisuutta suorittaa lääketieteen tutkinnon ulkomailla englanninkielisessä opetusohjelmassa? Niin olet oikeassa paikassa!</p>
           </div>
         </div>
+        </div>
+        </div>
+        </div>
+        <div className='myRomania'>
+        <h1>Miksi Romania ?</h1>
+        <div className='lääketiede-text-container'>
+        <div className="lääketiede-text1">
+          <h2>Mikä hakijoita houkuttelee ensisijaisesti ?</h2>
+          <h3>Hyvät sisäänpääsymahdollisuudet</h3>
+          <p>Sisäänpääsy Romanian lääketieteelliseen on huomattavasti todennäköisempää.</p>
+          <h3>Laadukas ja käytännönläheinen opetus</h3>
+          <p>EU:n vaatimusten mukainen koulutus ja käytännön läheisempi opetus.</p>
+          <h3>Edulliset lukuvuosimaksut</h3>
+          <p>Viron, Latvian sekä muut euroopan maat veloittavat jopa 2-3 kertaisia lukuvuosi maksuja. Romanian lukuvuosi maksu on n.5 000€</p>
+          <h3>Alhainen elintaso, joka sopii hyvin opiskelijalle.</h3>
+          <p>Maan yleisen alhaisen elintason vuoksi opintotuki kattaa sinun kuukauden kulut.</p>
         </div>
         </div>
         </div>
