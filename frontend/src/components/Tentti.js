49a57f896373ba5771d7e216961a4a26e266db6b
Agrin <agrins@metropolia.fi>
2023-11-08 14:48:43 +0200

    ff

--- frontend/src/components/Tentti.js
+++ frontend/src/components/Tentti.js
@@ -9,18 +9,16 @@ const Tentti = () => {
       <div>
         <div className="tentti">
         <Navbar showLinks={showLinks} setShowLinks={setShowLinks} />
-        <div className="tentti-card">
+        <div className="tentti-main">
         <h1>Tenttivaatimukset</h1>
-        <img
-            src="tentti-image.jpeg"
-            alt=""
-            className="tentti-card-pic"
-            loading="lazy"
-        />
+        <div className='tentti-card-container'>
+        <div className="tentti-card">
+        <div className="tentti-pic-frame">
+        <img src="tentti-image.jpeg" alt="" className="tentti-card-pic" loading="lazy"/>
         </div>
         <div className="outside-tentti">
           <div className="tenttivaatimus-text">
-              <p>Romanian arvostetuimissa yliopistoissa ei edellytetä kemiaa ja fysiikkaa valintakokeissa. Kemian ja fysiikan puolesta pääsykokeissa painotetaan anatomian ja fysiologian osaamista. Pääsykoe on tosi haasteellinen kaikissa yliopistoissa. Riippuen hakemaasi yliopistoon pääsykokeet ovat joko suullisia tai kirjallisia. Anatomia ja fysiologia ovat laajoja ja haastavia opintoja. Pääset koe tilaisuuksissa näyttämään vahvaa englannin kielen taitoa sekä asiantuntevaa osaamistasi anatomiassa sekä fysiologiassa</p>
+              <p>Romanian arvostetuimissa yliopistoissa ei edellytetä kemiaa ja fysiikkaa valintakokeissa. Kemian ja fysiikan puolesta pääsykokeissa painotetaan anatomian ja fysiologian osaamista. Hakemaasi yliopistoon pääsykokeet ovat joko suullisia tai kirjallisia. Anatomia ja fysiologia ovat laajoja ja haastavia opintoja. Pääset koe tilaisuuksissa näyttämään vahvaa englannin kielen taitoa sekä asiantuntevaa osaamistasi anatomiassa sekä fysiologiassa</p>
           </div>
         </div>
         </div>
@@ -36,6 +34,9 @@ const Tentti = () => {
           <h2>Anatomian & Fysiologian koe</h2>
             <p>Koe on suullinen ja hyvin laaja. Professorit voivat kysyä mitä tahansa koskien ihmisen anatomiaa tai physiologiaa. Kokeessa ei katsota ainoastaan aihealueiden osaamista vaan niiden esittämistä ammatti kielellä.</p>
             <p>Suullinen pääsykoe kestää koko päivän tyypillisesti klo 9-21. Tämä johtuu siitä, että kaikki hakijat täytyy ehtiä haastatella läpi. Pidä huoli siitä, että tiedät, milloin sinut kutsutaan haastatteluun ja missä välissä on taukoja. Koska odotukset kokeiden välillä kestävät useita tunteja. Odottaessa voit myös käydä lepäämässä, tankkaamassa tai lukemassa. Kirjallinen pääsykoe tilaisuus on suomalaisille opiskelijoille tuttua. Eli koe alkaa 9:ltä aamulla ja kestää klo 12:00 asti . Kokeet ovat monivalinta kysymyksiä anatomian ja physiologian opinnoista.</p>
+        </div>
+        </div>
+        </div>
         </div>
         </div>
         </div> 
