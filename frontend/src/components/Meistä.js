50db15f44abe208911d1154b4f13645de341cbe5
Agrin <agrins@metropolia.fi>
2023-11-10 20:04:14 +0200

    sssss

--- frontend/src/components/Meistä.js
+++ frontend/src/components/Meistä.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Navbar from './Navbar';
 import '../styles/Meistä.css';
 
+
 const Meistä = () => {
     const [showLinks, setShowLinks] = useState(false);
   
@@ -9,6 +10,12 @@ const Meistä = () => {
       <div>
         <div className="meistä">
         <Navbar showLinks={showLinks} setShowLinks={setShowLinks} />
+        <div className='meistä-pic-frame' style={{ backgroundImage: `url(/meistä.jpg)` }}>
+        <div className='meistä-main'>
+          <h1>Tukemassa kaltaistemme nuorien lääkäripolkua</h1>
+          <p>TUUTORIKESKUS Oy on vuonna 2016  perustettu täysin suomalainen osakeyhtiö kolmen lääketieteellisen opiskelijan toimesta. Meidän ydintehtävämme on yksinkertainen: tahdomme auttaa sinua saavuttamaan unelmasi.</p>
+        </div>
+        </div>
         </div>
     </div>
 
