7e81f087c261738e714b975940ad4fab9ce440c6
Agrin <agrins@metropolia.fi>
2023-11-17 18:40:46 +0200

    kkk

--- backend/controller/EnrollmentController.js
+++ backend/controller/EnrollmentController.js
@@ -1,49 +1,47 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
-const path = require('path'); // Import the path module
+const path = require('path');
 
+function sendEnroll(name, surname, birthday, email, phone, tutkinto, milloin, courseTitle, callback) {
+  const emailContent = `Ilmottautuiminen kurssille ${courseTitle}:\nEtunimi: ${name}\nSukunimi: ${surname}\nSyntymäpäivä: ${birthday}\nSähköposti: ${email}\nPuhelin: ${phone}\nTutkinto: ${tutkinto}\nValmistuu/Valmistunut: ${milloin}`;
 
-function sendEnroll(name, surname, birthday, email, phone, tutkinto, milloin, callback) {
-  const emailContent = `Ilmottautuiminen:\nEtunimi: ${name}\nSukunimi: ${surname}\nSyntymäpäivä: ${birthday}\nSähköposti: ${email}\nPuhelin: ${phone}\nTutkinto: ${tutkinto}\nValmistuu/Valmistunut: ${milloin}`;
+  const pdfFilePath = path.join(__dirname, 'contract.pdf');
 
-  const pdfFilePath = path.join(__dirname,'contract.pdf');
-
-const transporter = nodemailer.createTransport({
+  const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465, // Use 587 for TLS
-    secure: true, // For SSL
+    port: 465,
+    secure: true,
     auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS,
-  },
-});
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+  });
 
   const mailOptions = {
     from: email,
     to: process.env.GMAIL_USER,
-    subject: 'Ilmottautuminen',
-    text: emailContent
+    subject: `Ilmottautuminen kurssille ${courseTitle}`,
+    text: emailContent,
   };
 
   const enrollOptions = {
     from: process.env.GMAIL_USER,
     to: email,
-    subject: 'Ilmottautuminen',
+    subject: `Ilmottautuminen ${courseTitle} kurssille`,
     text: emailContent,
     attachments: [
       {
-        filename: 'contract.pdf', // Name of the attached file
-        path: pdfFilePath, // Path to the PDF file
+        filename: 'contract.pdf',
+        path: pdfFilePath,
       },
     ],
     html: `
       <p>Tervetuloa kurssillemme, ${name}!</p>
-      <p>Olemme iloisia, että päätit liittyä meidän kurssille.</p>
+      <p>Olemme iloisia, että päätit liittyä meidän ${courseTitle} kurssille:.</p>
       <p>Liitteenä on kurssisopimus (PDF-tiedosto), joka tulee täyttää ja palauttaa meille.</p>
       <p>Kiitos ja tervetuloa!</p>
     `,
   };
-  
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
@@ -51,7 +49,7 @@ const transporter = nodemailer.createTransport({
       callback(error, null);
     } else {
       console.log('Enroll sent: ' + info.response);
-      callback(null, { message: 'Enroll sent successfully' }); // Send a success message
+      callback(null, { message: 'Enroll sent successfully' });
     }
   });
 
