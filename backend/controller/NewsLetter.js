6f7af456f973108255289246c63152e38aed8506
Agrin <agrins@metropolia.fi>
2023-11-24 16:16:00 +0200

    ssisisi

--- backend/controller/NewsLetter.js
+++ backend/controller/NewsLetter.js
@@ -19,17 +19,24 @@ function sendNews(email, callback) {
     to: email,
     subject: `Tuutorikeskus uutiskirje`,
     html: `
-    <h2>Tervetuloa Lääkiksen pariin</h1>
-    <br />
+      <h2>Tervetuloa Lääkiksen pariin</h2>
+      <br />
       <h3>Miksi juuri Romaniaan?</h3>
-        <p>Suomessa lääkäriopintoihin hyväksytään vuosittain vain noin 10 % hakijoista. Romaniaan pääsi tänä vuonna 100 % Romanian lääketieteelliseen tiedekuntaan valmistautuvista kurssilaisistamme! Koska Romania on EU-maa, saa sieltä Valviran hyväksymän koulutuksen. Kaikki eurooppalaiset työmarkkinat ovat auki vielä paremmin kuin suomalaisella diplomilla; opiskeluhan on englanniksi.</p>
-        <p>Työssäoppimiseen pääset mukaan jo 1. opiskeluvuoden päätyttyä (Suomessa vasta 4. opiskeluvuonna).  Valmistuessasi sinulla on siis enemmän työkokemusta kuin Suomessa opiskelleilla vastavalmistuneilla lääkäreillä.</p>
-        <p>Opintojesi aikana luot kontakteja ympäri maailmaa tuleviin työtovereihin, esimiehiin ja työnantajiin. Avaat englanninkielisen koulutuksesi ansiosta myös sellaisia työmahdollisuuksia, jotka ovat Suomessa opiskelleille paljon vaikeammin saavutettavissa.</p>
-        <p>Hakemalla Romanian lääketieteelliseen sinulla on suuremmat sisäänpääsymahdollisuudet ensikertalaisena kuin Suomessa. Suomessa sisäänpääsyyn hakijat keskimäärin kuluttavat keskimäärin 2-3 yrityskertaa. Tämä tarkoittaa sitä, että säästät 2- 3 vuotta, jos pääset Romaniaan lääketieteelliseen opiskelemaan jo heti ensimmäisellä pääsykoekierroksella. Aika on todella arvokasta. </p>
-        <br />
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/BadnkcGYnzg?si=JVSE1K3SpCZltlwg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    `,  
+      <p>Suomessa lääkäriopintoihin hyväksytään vuosittain vain noin 10 % hakijoista. Romaniaan pääsi tänä vuonna 100 % Romanian lääketieteelliseen tiedekuntaan valmistautuvista kurssilaisistamme! Koska Romania on EU-maa, saa sieltä Valviran hyväksymän koulutuksen. Kaikki eurooppalaiset työmarkkinat ovat auki vielä paremmin kuin suomalaisella diplomilla; opiskeluhan on englanniksi.</p>
+      <p>Työssäoppimiseen pääset mukaan jo 1. opiskeluvuoden päätyttyä (Suomessa vasta 4. opiskeluvuonna). Valmistuessasi sinulla on siis enemmän työkokemusta kuin Suomessa opiskelleilla vastavalmistuneilla lääkäreillä.</p>
+      <p>Opintojesi aikana luot kontakteja ympäri maailmaa tuleviin työtovereihin, esimiehiin ja työnantajiin. Avaat englanninkielisen koulutuksesi ansiosta myös sellaisia työmahdollisuuksia, jotka ovat Suomessa opiskelleille paljon vaikeammin saavutettavissa.</p>
+      <p>Hakemalla Romanian lääketieteelliseen sinulla on suuremmat sisäänpääsymahdollisuudet ensikertalaisena kuin Suomessa. Suomessa sisäänpääsyyn hakijat keskimäärin kuluttavat keskimäärin 2-3 yrityskertaa. Tämä tarkoittaa sitä, että säästät 2-3 vuotta, jos pääset Romaniaan lääketieteelliseen opiskelemaan jo heti ensimmäisellä pääsykoekierroksella. Aika on todella arvokasta.</p>
+      <br />
+      <h3>YouTube Video</h3>
+      <a href="https://www.youtube.com/watch?v=BadnkcGYnzg" target="_blank">
+        <img src="THUMBNAIL_URL" alt="Video Thumbnail" width="560" height="315" style="display: block; margin: 0 auto;">
+        <div style="position: relative; top: -80px; text-align: center; color: #fff; font-size: 24px; cursor: pointer;">
+          ▶ Play Video
+        </div>
+      </a>
+    `,
   };
+  
 
   transporter.sendMail(newsOptions, (error, info) => {
     if (error) {
